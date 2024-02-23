289e28114529573a645eaf3ac528ced7e8efa416
MucahidDeveloper <Mucadad221@gmail.com>
2024-02-23 14:02:25 +0300

    Comment correction

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -9,7 +9,7 @@ import {
 import rateLimiter from "express-rate-limit";
 
 const apiLimiter = rateLimiter({
-  windowMs: 5 * 60 * 1000, // 15 minutes
+  windowMs: 5 * 60 * 1000, // 5 minutes
   max: 5,
   message: { msg: "IP rate limit exceeded, retry in 5 minutes." },
 });
